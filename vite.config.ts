88f0ebf49976909102ecabbbbed77d00531886c1
samruddhi <samruddhitathe48@gmail.com>
2026-02-10 12:11:41 +0530

    Images updated

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        proxy: {
+          '/v1': {
+            target: 'http://localhost:8080',
+            changeOrigin: true,
+          },
+        },
       },
       plugins: [react()],
       define: {
